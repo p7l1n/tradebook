d4bcd92a3f7d2cefc6f262c9f100713f15aa5b81
plapinp plapinp <plapinp17@gmail.com>
2025-03-26 23:26:30 +0300

    fix: remove proxy

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  devServer: {
-    proxy: {
-      '^/api': {
-        target: 'http://194.87.146.150:5000',
-        ws: true,
-        changeOrigin: true
-      },
-    }
-  },
   transpileDependencies: true,
   css: {
     loaderOptions: {
