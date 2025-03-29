de3e8db4d69ac674a86e371c093b058843dcc2b1
plapinp plapinp <plapinp17@gmail.com>
2025-03-29 10:42:11 +0300

    fix: remove https

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  devServer: {
-    // https: true,
-  },
   transpileDependencies: true,
   css: {
     loaderOptions: {
