d53922a5278c9bf5ee907a41b9d01548b9c999e0
plapinp plapinp <plapinp17@gmail.com>
2025-04-09 10:29:01 +0300

    fix: update version

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <MainMenu v-if="userInfo" />
-  <div class="app-version">{{ "p1.0.3" }}</div>
+  <div class="app-version">{{ "p1.0.4" }}</div>
   <router-view />
 </template>
 <script>
